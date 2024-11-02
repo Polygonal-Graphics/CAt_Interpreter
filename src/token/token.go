4c69ca939633ba34fb0b0d2c770acaab26cf4d12
Polygonal Graphics <154336865+Polygonal-Graphics@users.noreply.github.com>
2024-11-02 08:15:42 -0500

    Added REPL

--- src/token/token.go
+++ src/token/token.go
@@ -43,6 +43,7 @@ const (
 	IF       = "IF"
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
+	UWU      = "UWU"
 )
 
 var keywords = map[string]TokenType{
@@ -53,6 +54,7 @@ var keywords = map[string]TokenType{
 	"if":     IF,
 	"else":   ELSE,
 	"return": RETURN,
+	"uwu":    UWU,
 }
 
 func LookupIdent(ident string) TokenType {
